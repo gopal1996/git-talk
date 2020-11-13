87a41280f4e80362a3ae6a743a6fc717a758fc76
Gopalakrishnan <krishnan.gopal34@gmail.com>
2020-11-14 01:50:21 +0530

    Added Carousel

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>',
+        fade: true
+    });
+});
